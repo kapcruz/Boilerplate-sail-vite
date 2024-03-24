cf02ed9e8e72d0e4611fc0802bc781d27f880551
kapcruz <karina.pereira@feedz.com.br>
2024-03-24 19:58:32 -0300

    base pronta

---
+++ README.md
@@ -0,0 +1,11 @@
+## Getting Started
+
+First, run the development server:
+
+```bash
+sail up -d
+sail composer install
+sail npm install
+
+sail npm run dev
+```
---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0 text-sky-500 hover:text-sky-600">
+    <h1>{{ greeting }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    setup: () => ({
+        greeting: 'Hello World from Vue!'
+    })
+}
+</script>
